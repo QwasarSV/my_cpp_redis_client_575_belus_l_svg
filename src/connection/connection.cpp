afa3334c78ad77321fdf83caeb3e9792f3eedd07
home_server <lorris.belus@icloud.com>
2024-02-22 16:25:22 +0000

    Reorganize structure

--- src/connection/connection.cpp
+++ src/connection/connection.cpp
@@ -1,112 +1,84 @@
 #include <main_header.hpp>
 
-// void test_redis_connection()
-// {
-//     redisContext *c = redisConnect("127.0.0.1", 6379);
-//     if (c == NULL || c->err)
-//     {
-//         if (c)
-//         {
-//             printf("Error: %s\n", c->errstr);
-//             // handle error
-//         }
-//         else
-//         {
-//             printf("Can't allocate redis context\n");
-//         }
-//     }
-// }
+namespace my_redis
+{
+    int RedisClient::getAdressType(const std::string& address)
+    {
+        if (address.rfind(TCP_STR, 0) == 0)
+        {
+            return TCP_ADDR;
+        }
+        else if (!address.empty() && std::isdigit(address[0]))
+        {
+            return IP_ADDR;
+        }
+        return INVALID;
+    }
 
-// void test_redis_connection_opt()
-// {
-//     redisOptions opt = {0};
-//     redisContext *c;
-//     REDIS_OPTIONS_SET_TCP(&opt, "myredis", 6379);
-//     opt.options |= REDIS_OPT_PREFER_IPV4;
-//     c = redisConnectWithOptions(&opt);
-//     if (c == NULL || c->err)
-//     {
-//         if (c)
-//         {
-//             printf("Error: %s\n", c->errstr);
-//         }
-//         else
-//         {
-//             printf("Can't allocate redis context\n");
-//         }
-//     }
-//     else
-//     {
-//         printf("Successfully connected to Redis\n");
-//     }
-//     if (c) redisFree(c);
-// }
+    void RedisClient::connection(const std::string& address)
+    {
+        splitHostAndPort(address, host, port);
+        context = redisConnect(host.c_str(), port);
+        if (context == NULL || context->err)
+        {
+            if (context)
+            {
+                printf("Error: %s\n", context->errstr);
+            }
+            else
+            {
+                printf("Can't allocate redis context\n");
+            }
+        }
+    }
 
-// void set_value(redisContext *c, const char *key, const char *value)
-// {
-//     redisReply *reply;
-//     reply = (redisReply*)redisCommand(c, "SET %s %s", key, value);
-//     if (reply == NULL)
-//     {
-//         printf("Error: %s\n", c->errstr);
-//         redisFree(c);
-//         exit(1);
-//     }
-//     if (reply->type == REDIS_REPLY_STATUS)
-//     {
-//         printf("SET operation - %s\n", reply->str);
-//     }
-//     else
-//     {
-//         printf("SET operation failed.\n");
-//     }
-//     freeReplyObject(reply);
-// }
+    void RedisClient::connection_opt(const std::string& address)
+    {
+        splitHostAndPort(address, host, port);
+        redisOptions opt = {0};
+        REDIS_OPTIONS_SET_TCP(&opt, host.c_str(), port);
+        opt.options |= REDIS_OPT_PREFER_IPV4;
+        context = redisConnectWithOptions(&opt);
+        if (context == NULL || context->err)
+        {
+            if (context)
+            {
+                printf("Error: %s\n", context->errstr);
+            }
+            else
+            {
+                printf("Can't allocate redis context\n");
+            }
+        }
+        else
+        {
+            printf("Successfully connected to Redis\n");
+        }
+    }
 
-// void get_value(redisContext *c, const char *key)
-// {
-//     redisReply* reply;
-//     reply = (redisReply*)redisCommand(c, "GET %s", key);
-//     if (reply == NULL)
-//     {
-//         printf("Error: %s\n", c->errstr);
-//         redisFree(c);
-//         exit(1);
-//     }
-//     if (reply->type == REDIS_REPLY_STRING)
-//     {
-//         printf("GET operation - value: %s\n", reply->str);
-//     }
-//     else
-//     {
-//         printf("GET operation failed or key does not exist.\n");
-//     }
-//     freeReplyObject(reply);
-// }
-
-// void test_redis_get_and_set_opt()
-// {
-//     redisOptions opt = {0};
-//     redisContext *c;
-//     REDIS_OPTIONS_SET_TCP(&opt, "myredis", 6379);
-//     opt.options |= REDIS_OPT_PREFER_IPV4;
-//     c = redisConnectWithOptions(&opt);
-//     set_value(c, "Hiredis01", "Hiredis01_value");
-//     if (c == NULL || c->err)
-//     {
-//         if (c)
-//         {
-//             printf("Error: %s\n", c->errstr);
-//         }
-//         else
-//         {
-//             printf("Can't allocate redis context\n");
-//         }
-//     }
-//     else
-//     {
-//         printf("Successfully connected to Redis\n");
-//     }
-//     get_value(c, "Hiredis01");
-//     if (c) redisFree(c);
-// }
+    void RedisClient::splitHostAndPort(const std::string& input, std::string& host, int& port)
+    {
+        size_t protocolPos = input.find("://");
+        size_t startPos = 0;
+        if (protocolPos != std::string::npos)
+        {
+            startPos = protocolPos + 3;
+        }
+        size_t colonPos = input.find(':', startPos);
+        if (colonPos == std::string::npos)
+        {
+            throw std::invalid_argument("Input string does not contain a port separator ':'");
+        }
+        host = input.substr(startPos, colonPos - startPos);
+        std::string portStr = input.substr(colonPos + 1);
+        if (portStr.empty() || portStr.find_first_not_of("0123456789") != std::string::npos)
+        {
+            throw std::invalid_argument("Port must be a number");
+        }
+        port = std::stoi(portStr);
+        if (port < 0 || port > 65535)
+        {
+            throw std::invalid_argument("Port number is out of valid range");
+        }
+    }
+}
